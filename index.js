9fbd48c672262f9ce968d915d9eb98e8cb89cdbc
Dlowyz <166107299+Dlowyz@users.noreply.github.com>
2025-05-15 15:54:50 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize('mysql://root:1234@localhost:3306/joga_sequelize')
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to database')
+    })
+    .catch(err => {
+        console.log('cant connect to database: ', err)
+    })
+
+
 app.get('/', (req, res) => {
     res.json({message: "Welcome to sequelize!"})
 })
